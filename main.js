16a4ede5a4f80fd11900c2e2682b1f28c2a436dc
njs04210 <pearlnkim@gmail.com>
2020-08-10 01:28:20 +0900

    MySQL JOIN - 글 수정 구현

--- main.js
+++ main.js
@@ -117,10 +117,10 @@ var app = http.createServer(function (request, response) {
         if (error2) {
           throw error2;
         }
-
-        var list = template.list(topics);
-        var html = template.HTML(topic[0].title, list,
-          `
+        db.query(`SELECT * FROM author`, function (error3, authors) {
+          var list = template.list(topics);
+          var html = template.HTML(topic[0].title, list,
+            `
             <form action="/update_process" method="post">
               <input type="hidden" name="id" value="${topic[0].id}">
               <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
@@ -128,15 +128,19 @@ var app = http.createServer(function (request, response) {
                 <textarea name="description" placeholder="description">${topic[0].description}</textarea>
               </p>
               <p>
+                ${template.authorSelect(authors, topic[0].author_id)}
+              </p>
+              <p>
                 <input type="submit">
               </p>
             </form>
             `,
-          `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
-        );
-        response.writeHead(200); //웹페이지로써 꼭 알려줘야하는 중요한 코드
-        response.end(html);
-      })
+            `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
+          );
+          response.writeHead(200); //웹페이지로써 꼭 알려줘야하는 중요한 코드
+          response.end(html);
+        });
+      });
     });
 
   } else if (pathname === '/update_process') {//update하고 제출버튼 눌렀을 때
@@ -147,7 +151,7 @@ var app = http.createServer(function (request, response) {
     request.on('end', function () {
       var post = qs.parse(body);
       db.query(`
-      UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?`, [post.title, post.description, post.id], function (error, result) {
+      UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`, [post.title, post.description, post.author, post.id], function (error, result) {
         response.writeHead(302, { Location: `/?id=${post.id}` });
         response.end();
       }
