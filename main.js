83f629f1f561a42b5e68670409391a5fa1521fca
njs04210 <pearlnkim@gmail.com>
2020-08-10 19:22:29 +0900

    정리정돈 - topic 2, 3

--- main.js
+++ main.js
@@ -13,146 +13,19 @@ var app = http.createServer(function (request, response) {
     //home일때의 경우 . web눌렀을 때. 홈으로 들어왔을 때.
     if (queryData.id === undefined) {
       topic.home(request, response);
-    } else {//HTML, CSS, JavaScript 눌렀을때. id값이 있는 애
-      db.query(`SELECT * FROM topic`, function (error, topics) {
-        if (error) {
-          throw error;
-        }
-        db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id = author.id WHERE topic.id=?`, [queryData.id], function (error2, topic) {
-          if (error2) {
-            throw error2;
-          }
-          var title = topic[0].title;
-          var description = topic[0].description;
-          var list = template.list(topics);
-          var html = template.HTML(title, list,
-            `<h2>${title}</h2>
-            ${description}
-            <p>by ${topic[0].name}</p>`,
-            ` <a href="/create">create</a>
-                <a href="/update?id=${queryData.id}">update</a>
-                <form action="delete_process" method="post">
-                  <input type="hidden" name="id" value="${queryData.id}">
-                  <input type="submit" value="delete">
-                </form>`
-          );
-          response.writeHead(200); //웹페이지로써 꼭 알려줘야하는 중요한 코드
-          response.end(html);
-        })
-      });
+    } else {//HTML, CSS, JavaScript 눌렀을때. 선택한 목록 상세보기. 
+      topic.page(request, response);
     }
   } else if (pathname === '/create') {//create 눌렀을 때
-    db.query(`SELECT * FROM topic`, function (error, topics) {
-      db.query(`SELECT * FROM author`, function (error2, authors) {
-
-        var title = 'Create';
-        var list = template.list(topics);
-        var html = template.HTML(title, list,
-          `
-            <form action="/create_process" method="post">
-              <p><input type="text" name="title" placeholder="title"></p>
-              <p>
-                <textarea name="description" placeholder="description"></textarea>
-              </p>
-              <p>
-                ${template.authorSelect(authors)}
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-          `<a href="/create">create</a>`
-        );
-        response.writeHead(200); //웹페이지로써 꼭 알려줘야하는 중요한 코드
-        response.end(html);
-      });
-    });
+    topic.create(request, response);
   } else if (pathname === '/create_process') {//create하고 제출 버튼 눌렀을 때
-    var body = '';
-    request.on('data', function (data) {
-      body = body + data;
-    });
-    request.on('end', function () {
-      var post = qs.parse(body);
-      db.query(`
-      INSERT INTO topic (title, description, created, author_id)
-        VALUES (?, ?, NOW(), ?)`,
-        [post.title, post.description, post.author], function (error, result) {
-          if (error) {
-            throw error;
-          }
-          response.writeHead(302, { Location: `/?id=${result.insertId}` });
-          response.end();
-        })
-
-    });
-
+    topic.create_process(request, response);
   } else if (pathname === '/update') {  //update를 눌렀을 때
-    db.query(`SELECT * FROM topic`, function (error, topics) {
-      if (error) {
-        throw error;
-      }
-      db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function (error2, topic) {
-        if (error2) {
-          throw error2;
-        }
-        db.query(`SELECT * FROM author`, function (error3, authors) {
-          var list = template.list(topics);
-          var html = template.HTML(topic[0].title, list,
-            `
-            <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${topic[0].id}">
-              <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${topic[0].description}</textarea>
-              </p>
-              <p>
-                ${template.authorSelect(authors, topic[0].author_id)}
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-            `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
-          );
-          response.writeHead(200); //웹페이지로써 꼭 알려줘야하는 중요한 코드
-          response.end(html);
-        });
-      });
-    });
-
+    topic.update(request, response);
   } else if (pathname === '/update_process') {//update하고 제출버튼 눌렀을 때
-    var body = '';
-    request.on('data', function (data) {
-      body = body + data;
-    });
-    request.on('end', function () {
-      var post = qs.parse(body);
-      db.query(`
-      UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`, [post.title, post.description, post.author, post.id], function (error, result) {
-        response.writeHead(302, { Location: `/?id=${post.id}` });
-        response.end();
-      }
-      )
-
-    });
+    topic.update_process(request, response);
   } else if (pathname === '/delete_process') {//delete 눌렀을 때
-    var body = '';
-    request.on('data', function (data) {
-      body = body + data;
-    });
-    request.on('end', function () {
-      var post = qs.parse(body);
-      db.query(`DELETE FROM topic WHERE id=?`, [post.id], function (error, result) {
-        if (error) {
-          throw error;
-        }
-        response.writeHead(302, { Location: `/` });
-        response.end();
-      });
-    });
+    topic.delete_process(request, response);
   } else {
     response.writeHead(404);
     response.end('Not found');
